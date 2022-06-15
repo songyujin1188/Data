8381f0ed253774558f74c731deae150f89a80ae8
songyujin1188 <90204322+songyujin1188@users.noreply.github.com>
2022-06-15 11:31:31 -0400

    Update Web scrape.py

--- Web scrape.py
+++ Web scrape.py
@@ -7,17 +7,18 @@ html = requests.get(url)
 soup = BeautifulSoup(html.content,'html.parser')
 lists = soup.find_all('section', class_="listing-search-item")
 
-with open('house.csv', 'w',encoding='utf-8', newline='') as f:
+with open("house.csv", 'w',encoding='utf-8', newline='') as f:
     write = writer(f)
     head = ['Title', 'Location', 'Price', 'Area', 'Rooms', 'Interior']
     write.writerow(head)
     for list in lists:      
-        title = lists.find('a', class_="listing-search-item__link--title").text.replace('\n','')
-        location = lists.find('div', class_="listing-search-item__location").text.replace('\n','')
-        price = lists.find('div', class_="listing-search-item__price").text.replace('\n','')
-        features_surface_area = lists.find('li', class_="illustrated-features__item illustrated-features__item--surface-area").text.replace('\n','')
-        features_number_of_rooms = lists.find('li', class_="illustrated-features__item illustrated-features__item--number-of-rooms").text.replace('\n','')
-        features_interior = lists.find('li', class_="illustrated-features__item illustrated-features__item--interior").text.replace('\n','')
+        title = list.find('a', class_="listing-search-item__link--title").text.replace('\n','')
+        location = list.find('div', class_="listing-search-item__location").text.replace('\n','')
+        price = list.find('div', class_="listing-search-item__price").text.replace('\n','')
+        features_surface_area = list.find('li', class_="illustrated-features__item illustrated-features__item--surface-area").text.replace('\n','')
+        features_number_of_rooms = list.find('li', class_="illustrated-features__item illustrated-features__item--number-of-rooms").text.replace('\n','')
+        features_interior = list.find('li', class_="illustrated-features__item illustrated-features__item--interior")
+        features_interior = 'None' if features_interior is None else features_interior.text.replace('\n','')
         form = [title, location, price, features_surface_area, features_number_of_rooms,features_interior]
         write.writerow(form)
     
